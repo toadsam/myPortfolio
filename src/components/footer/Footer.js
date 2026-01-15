6a0bc2c8aaf34c0c01ce6b7ce0841875a6b3cd1a
toadsam <toadsam@naver.com>
2026-01-16 04:33:34 +0900

    [ADD] 템플릿 내용 수정 -재훈

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,12 +10,12 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by DeveloperFolio Team")}
+          {emoji("정재훈 | Full-Stack Developer · Unity Developer")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
           <a
-            href="https://github.com/saadpasta/developerFolio"
+            href="https://github.com/toadsam"
             target="_blank"
             rel="noreferrer"
           >
